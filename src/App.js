fd7a805e60f1d7a23052a7c660d361a368b2f8c3
cninz <nchua.nc47@gmail.com>
2023-07-02 19:09:57 -0600

    Week 1 Assignment - React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import Header from './components/Header';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
