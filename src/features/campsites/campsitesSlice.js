64a27463361528f07bbf142d1f081b856a0e48f9
cninz <nchua.nc47@gmail.com>
2023-07-21 19:10:45 -0600

    Campsites useSelector

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,26 +1,27 @@
-import{ CAMPSITES} from '../../app/shared/CAMPSITES'
 import { createSlice } from '@reduxjs/toolkit'
-
-export const selectAllCampsites = () => {
-    return CAMPSITES
-}
-
-export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
-}
-
-export const selectFeaturedCampsite = () => {
-    return CAMPSITES.find((campsite) => campsite.featured)
-}
+import{ CAMPSITES} from '../../app/shared/CAMPSITES'
 
 const initialState = {
     campsitesArray: CAMPSITES
-}
+};
 
 const campsitesSlice = createSlice({
     name: 'campsites', 
     initialState
-
-})
+});
 
 export const campsitesReducer = campsitesSlice.reducer
+
+export const selectAllCampsites = (state) => {
+    return state.campsites.campsitesArray
+};
+
+export const selectCampsiteById = (id) => (state) => {
+    return state.campsites.campsitesArray.find(
+        (campsite) => campsite.id === parseInt(id)
+    );
+};
+
+export const selectFeaturedCampsite = (state) => {
+    return state.campsites.campsitesArray.find((campsite) => campsite.featured);
+};
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,12 +1,19 @@
 import { Col, Row } from 'reactstrap';
 // import DisplayCard from './DisplayCard';
+import { useSelector } from 'react-redux';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 
 const DisplayList = () => {
-    const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()]
+    const items = useSelector((state)=>[
+        selectFeaturedCampsite(state), 
+        selectFeaturedPromotion(state), 
+        selectFeaturedPartner(state)
+    ]);
+
+    console.log('disaplay items: ', items)
 
     return(
         <Row>
