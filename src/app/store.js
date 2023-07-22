c4adef133cd9a800807dc959a424228d6acae485
cninz <nchua.nc47@gmail.com>
2023-07-22 13:37:11 -0600

    Redux for All

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
+import { commentsReducer } from '../features/comments/commentsSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
+    comments: commentsReducer, 
+    partners: partnersReducer, 
+    promotions: promotionsReducer
   },
 });
+
