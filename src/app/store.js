2021ba7dbffedeb0b9118604689d9a48db863126
cninz <nchua.nc47@gmail.com>
2023-07-27 20:02:27 -0600

    Assignment 4

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,15 @@ import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import logger from 'redux-logger';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer, 
     partners: partnersReducer, 
-    promotions: promotionsReducer
+    promotions: promotionsReducer,
+    user: userReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
   
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,43 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from '../app/assets/img/logo.png';
-import {useState} from 'react';
+import { useState } from 'react';
 import { Collapse, NavbarToggler, Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import UserLoginForm from '../features/user/UserLoginForm';
 
 const Header = () => {
-    const  [menuOpen, setMenuOpen] =  useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <Navbar dark color='primary' sticky='top' expand='md'>
             <NavbarBrand className='ms-5' href='/'>
                 <img src={NucampLogo} alt='nucamp logo' />
                 <h1 className='mt-1' element='float-start'>NuCamp</h1>
             </NavbarBrand>
-            <NavbarToggler onClick ={ () => setMenuOpen(!menuOpen)}/>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
             <Collapse navbar isOpen={menuOpen}>
-            <Nav className='ms-auto' navbar>
+                <Nav className='ms-auto' navbar>
                     <NavItem>
                         <NavLink className='nav-link' to='/'>
-                            <i className='fa fa-home fa-lg'/> Home
+                            <i className='fa fa-home fa-lg' /> Home
                         </NavLink>
                     </NavItem>
-                <NavItem>
-                    <NavLink className='nav-link' to='/directory'>
-                        <i className='fa fa-list fa-lg'/> Directory
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className='nav-link' to='/about'>
-                        <i className='fa fa-info fa-lg'/> About  
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink className='nav-link' to='/contact'>
-                        <i className='fa fa-address-card fa-lg'/> Contact
-                    </NavLink>
-                </NavItem>
-            </Nav>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/directory'>
+                            <i className='fa fa-list fa-lg' /> Directory
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/about'>
+                            <i className='fa fa-info fa-lg' /> About
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/contact'>
+                            <i className='fa fa-address-card fa-lg' /> Contact
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <UserLoginForm/>
             </Collapse>
         </Navbar>
     )
